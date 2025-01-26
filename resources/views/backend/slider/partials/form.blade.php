b980462b2f329b515319ea6fec1b388aef3c75e9
Ritu Gubhaju <ritu@cloudtechservice.com>
2025-01-26 20:04:42 +0545

    updates in form slider

--- resources/views/backend/slider/partials/form.blade.php
+++ resources/views/backend/slider/partials/form.blade.php
@@ -1,79 +1,74 @@
 @csrf
 <div class="row">
-    <div class="col-sm-8">
+    <div class="col-sm-12">
         <div class="card">
             <div class="card-underline">
-                <div class="card-head">
-                    <header class="ml-3 mt-2">{!! $header !!}</header>
+                <div class="card-head p-4">
+                    <header>{!! $header !!}</header>
                 </div>
                 <div class="card-body">
+
+
                     <div class="row">
                         <div class="col-sm-12">
                             <div class="form-group">
-                                <label for="" class="col-form-label pt-0">Title</label>
-                                    <div class="">
-                                        <input class="form-control" type="text" required name="title" value="{{ old('title', isset($slider->title) ? $slider->title : '') }}" placeholder="Enter Title">
-                                    </div>
-                                    <span id="textarea1-error" class="text-danger">{{ $errors->first('title') }}</span>
+                                <label for="Name">Name</label>
+                                <input type="text" name="title" class="form-control" required
+                                       value="{{ old('title', isset($slider->title) ? $slider->title : '') }}"/>
+
+                                <span id="textarea1-error" class="text-danger">{{ $errors->first('title') }}</span>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
+
+                    <div class="row mt-3">
                         <div class="col-sm-12">
                             <div class="form-group">
-                                <label for="" class="col-form-label pt-0">Caption</label>
-                                    <div class="">
-                                        <input class="form-control" type="text" required name="caption" value="{{ old('caption', isset($slider->caption) ? $slider->caption : '') }}" placeholder="Enter Caption">
-                                    </div>
+                                <label for="caption">caption</label>
+                                <textarea type="text" name="caption" class="form-control">{{old('caption',isset($slider->caption)?$slider->caption : '')}}</textarea>
+
                             </div>
                         </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-sm-12">
-                             <div class="form-group">
-                                <label for="" class="col-form-label pt-0">Link URL</label>
-                                    <div class="">
-                                        <input class="form-control" type="text" required name="link_url" value="{{ old('link_url', isset($slider->link_url) ? $slider->link_url : '') }}" placeholder="Enter Link URL">
-                                    </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-
-                    <div class="row">
+                    <div class="row mt-3">
                         <div class="col-sm-12">
                             <label class="text-default-light">Featured Image</label>
                             @if(isset($slider) && $slider->image)
-                                <input type="file" name="image" class="dropify" data-default-file="{{ asset(str_replace('\\', '/', $slider->image)) }}"/>
+                                <input type="file" name="image" class="dropify" id="input-file-events"
+                                       data-default-file="{{ asset(str_replace('\\', '/', $slider->image)) }}"/>
+
                             @else
                                 <input type="file" name="image" class="dropify"/>
                             @endif
+                            <input type="hidden" name="removeimage" id="removeimage" value=""/>
+                        </div>
+                        <!--<div class="col-sm-6">-->
+                        <!--    <label class="text-default-light">Banner Image(Optional)</label>-->
+                        <!--        <input type="file" name="banner_image" class="dropify" id="input-file-events"-->
+                        <!--    <input type="hidden" name="removeimage" id="removeimage" value=""/>-->
+                        <!--</div>-->
+                    </div>
+                    <hr>
+                    <div class="mt-3">
+                        <div class="d-flex justify-content-center">
+                            <input type="submit" name="sliderSubmit" class="btn btn-primary waves-effect waves-light me-1">
+                            <a class="btn btn-secondary waves-effect" href="{{ route('slider.index') }}">
+                                Back
+                            </a>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <div class="col-sm-4">
-        <div class="card card-affix affix-4">
-            <div class="card-head">
-                <div class="tools">
-                    <a class="btn btn-default btn-ink" href="{{ route('slider.index') }}">
-                        <i class="md md-arrow-back"></i>
-                        Back
-                    </a>
-                </div>
-            </div>
-            <div class="card-actionbar">
-                <div class="card-actionbar-row">
-                    <button type="reset" class="btn btn-default ink-reaction">Reset</button>
-                    {{--<input type="submit" name="draft" class="btn btn-info ink-reaction" value="Save Draft">--}}
-                    <input type="submit" name="publish" class="btn btn-primary ink-reaction" value="{{ isset($photo) && $photo->is_published ? 'Save' : 'Publish' }}">
                 </div>
             </div>
         </div>
     </div>
 </div>
+@push('scripts')
+    <!-- tinymce js -->
+<script src="{{ asset('backend/assets/libs/tinymce/tinymce.min.js') }}"></script>
+
+<!-- init js -->
+<script src="{{ asset('backend/assets/js/sliders/form-editor.init.js') }}"></script>
+
+@endpush
